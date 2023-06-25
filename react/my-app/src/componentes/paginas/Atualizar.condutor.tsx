9317abab5e938c915517a05596b6df43c4888cf9
TeixeiraSantoss <128059526+TeixeiraSantoss@users.noreply.github.com>
2023-06-25 18:51:26 -0300

    Update Atualizar.condutor.tsx

--- react/my-app/src/componentes/paginas/Atualizar.condutor.tsx
+++ react/my-app/src/componentes/paginas/Atualizar.condutor.tsx
@@ -1,84 +1,92 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Condutor } from "../../models/condutor.model";
+import { useParams } from "react-router-dom";
 
 export function AtualizarCondutor(){
+    const [Cpf, setCpf] = useState("");
     const [nome, setNome] = useState("");
     const [idade, setIdade] = useState("");
     const [dinheiro, setDinheiro] = useState("");
     const [alugando, setAlugando] = useState("");
     
-    function atualizar(){
-        let condutor: Condutor = new Condutor;
-        condutor.nome = nome;
-        condutor.idade = Number.parseInt(idade);
-        condutor.dinheiro = Number.parseInt(dinheiro);
-        condutor.alugando = Boolean(alugando);
-        
+    const { cpf } = useParams();
 
+    useEffect(() => {
+        // Buscar os dados do veículo com o ID fornecido
         axios
-            .post("http://localhost:3001/condutor/alterar", condutor)
-            .then((resposta: any) => {
-                //Executar algo quando a requisição for bem sucedida
-                //Códigos HTTP na faixa do 200
-                //Redirecionar para o componente da listagem
-                console.log(resposta.data.mensagem);
-                setNome("");
-                setIdade("");
-                setDinheiro("");
-                setAlugando("");
-            })
-            .catch((erro: any) => {
-                //Executar algo quando a requisição for mal sucedida
-                //Códigos HTTP na faixa do 400 e 500
-                console.log(erro);
-            });
-    }
+          .get(`http://localhost:3001/condutor/buscar/${cpf}`)
+          .then((resposta) => {
+            const condutor = resposta.data;
+            setNome(condutor.nome);
+            setIdade(condutor.idade.toString());
+            setDinheiro(condutor.dinheiro.toString());
+            setAlugando(condutor.alugando.toString());
+          })
+          .catch((erro) => {
+            console.log(erro);
+          });
+      }, [cpf]);
 
-    function teste(){
-        console.log("Testes");
+    function atualizar(){
+            let condutor = new Condutor();
+            condutor.cpf = Number.parseInt(Cpf);
+            condutor.nome = nome;
+            condutor.idade = Number.parseInt(idade);
+            condutor.dinheiro = Number.parseInt(dinheiro);
+            condutor.alugando = Boolean(alugando);
+            
+            axios
+            .post(`http://localhost:3001/condutor/alterar/${cpf}`, condutor)
+            .then((resposta) => {
+              console.log(resposta.data.mensagem);
+              console.log("Alterado comsucesso");
+            })
+            .catch((erro) => {
+              console.log(erro);
+              console.log("Erro ao alterar");
+        });
     }
 
     return(
         <div>
-                    <h1> Atualizar Condutor </h1>
-            <div>
-                <label>Nome:</label>
-                <input
-                type="text"
-                onChange={(event: any) => setNome(event.target.value)}
-                />
-            </div>
-
-            <div>
-                <label>Idade:</label>
-                <input
-                type="text"
-                onChange={(event: any) => setIdade(event.target.value)}
-                />
-            </div>
-
-            <div>
-                <label>Saldo:</label>
-                <input
-                type="text"
-                onChange={(event: any) => setDinheiro(event.target.value)}
-                />
-            </div>
-
-            <div>
-                <label>Alugando um veiculo:</label>
-                <input
-                type="text"
-                onChange={(event: any) => setAlugando(event.target.value)}
-                />
-            </div>
-
-            <div>
-                <button onClick={atualizar}>
-                Atualizar
-                </button>
-            </div>
+          <h1>Atualizar Condutor</h1>
+    
+          <div>
+            <label>Nome:</label>
+            <input
+              type="text"
+              onChange={(event) => setNome(event.target.value)}
+            />
+          </div>
+    
+          <div>
+            <label>Idade:</label>
+            <input
+              type="text"
+              onChange={(event) => setIdade(event.target.value)}
+            />
+          </div>
+    
+          <div>
+            <label>Dinheiro:</label>
+            <input
+              type="text"
+              onChange={(event) => setDinheiro(event.target.value)}
+            />
+          </div>
+    
+          <div>
+            <label>Alugando:</label>
+            <input
+              type="text"
+              onChange={(event) => setAlugando(event.target.value)}
+            />
+          </div>
+    
+          <div>
+            <button onClick={atualizar}>Atualizar</button>
+          </div>
         </div>
     )
 }
